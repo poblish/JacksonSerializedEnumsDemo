e30caff2ee5803e2b6b16d3d3141ec13c40df007
Andrew Regan <aregan@gmail.com>
2019-12-16 23:18:32 +0000

    Hamcrest => AssertJ

--- src/test/java/org/hiatusuk/SerializationTest.java
+++ src/test/java/org/hiatusuk/SerializationTest.java
@@ -1,9 +1,5 @@
 package org.hiatusuk;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.startsWith;
-
 import java.io.IOException;
 
 import org.junit.Assert;
@@ -13,6 +9,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SerializationTest {
 
     private final static ObjectMapper MAPPER = new ObjectMapper();
@@ -20,41 +18,41 @@ public class SerializationTest {
     @Test
     public void testSerializedOutputActive() throws JsonProcessingException {
         String serd = MAPPER.writeValueAsString( new Container("Foo", Status.ACTIVE, "Bar") );
-        assertThat(serd, equalTo("{\"name\":\"Foo\",\"status\":\"Active state\",\"other\":\"Bar\"}"));
+        assertThat(serd).isEqualTo("{\"name\":\"Foo\",\"status\":\"Active state\",\"other\":\"Bar\"}");
     }
 
     @Test
     public void testSerializedOutputUnknown() throws JsonProcessingException {
         String serd = MAPPER.writeValueAsString( new Container("Foo", Status.UNKNOWN, "Bar") );
-        assertThat(serd, equalTo("{\"name\":\"Foo\",\"status\":\"???\",\"other\":\"Bar\"}"));
+        assertThat(serd).isEqualTo("{\"name\":\"Foo\",\"status\":\"???\",\"other\":\"Bar\"}");
     }
 
     @Test
     public void testSerializedOutputCancelled() throws JsonProcessingException {
         String serd = MAPPER.writeValueAsString( new Container("Foo", Status.CANCELLED_EXPIRED, "Bar") );
-        assertThat(serd, equalTo("{\"name\":\"Foo\",\"status\":\"CANCELLED_EXPIRED\",\"other\":\"Bar\"}"));
+        assertThat(serd).isEqualTo("{\"name\":\"Foo\",\"status\":\"CANCELLED_EXPIRED\",\"other\":\"Bar\"}");
     }
 
     @Test
     public void testDeserActive() throws IOException {
         Container ctr = MAPPER.readValue("{\"name\":\"Foo\",\"status\":\"Active state\",\"other\":\"Bar\"}", Container.class);
-        assertThat(ctr.getName(), equalTo("Foo"));
-        assertThat(ctr.getStatus(), equalTo(Status.ACTIVE));
-        assertThat(ctr.getOther(), equalTo("Bar"));
+        assertThat(ctr.getName()).isEqualTo("Foo");
+        assertThat(ctr.getStatus()).isEqualTo(Status.ACTIVE);
+        assertThat(ctr.getOther()).isEqualTo("Bar");
     }
 
     @Test
     public void testDeserUnknown() throws IOException {
         Container ctr = MAPPER.readValue("{\"name\":\"Foo\",\"status\":\"???\",\"other\":\"Bar\"}", Container.class);
-        assertThat(ctr.getName(), equalTo("Foo"));
-        assertThat(ctr.getStatus(), equalTo(Status.UNKNOWN));
-        assertThat(ctr.getOther(), equalTo("Bar"));
+        assertThat(ctr.getName()).isEqualTo("Foo");
+        assertThat(ctr.getStatus()).isEqualTo(Status.UNKNOWN);
+        assertThat(ctr.getOther()).isEqualTo("Bar");
     }
 
     @Test
     public void testDeserWithDefaultedStatus() throws IOException {
         Container ctr = MAPPER.readValue("{\"name\":\"Foo\",\"other\":\"Bar\"}", Container.class);
-        assertThat(ctr.getStatus(), equalTo(Status.UNKNOWN));
+        assertThat(ctr.getStatus()).isEqualTo(Status.UNKNOWN);
     }
 
     @Test
@@ -64,7 +62,7 @@ public class SerializationTest {
             Assert.fail("Shoould have failed");
         }
         catch (InvalidFormatException e) {
-            assertThat(e.getMessage(), startsWith("Cannot deserialize value"));
+            assertThat(e.getMessage()).startsWith("Cannot deserialize value");
         }
     }
 }
